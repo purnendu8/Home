a428e939b5a696f139c7e8c76304d201a8a9bf83
Purnendu Kumar <purnendukumar8@hotmail.com>
2020-06-28 20:45:41 +0530

    4th commit cloned updated

--- jobportal/src/main/java/pageObjects/Baytpage.java
+++ jobportal/src/main/java/pageObjects/Baytpage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Baytpage {
-	@FindBy(linkText="Log In")
-	private WebElement login;
+	//@FindBy(linkText="Log In")
+	//private WebElement login;
 
 }
