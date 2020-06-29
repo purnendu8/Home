cda5856931896b823dbb46ca43f11163a47bca9d
Purnendu Kumar <purnendukumar8@hotmail.com>
2020-06-29 15:13:15 +0530

    bayt page class is added

--- jobportal/src/main/java/pageObjects/Baytpage.java
+++ jobportal/src/main/java/pageObjects/Baytpage.java
@@ -1,10 +1,23 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Baytpage {
 	@FindBy(linkText="Log In")
-	private WebElement login;
+	private WebElement loginLink;
+	
+	public Baytpage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void loginLink()
+	{
+		loginLink.click();
+	}
+	
 
 }
