13878e873f1c1eeb7826e3540c4855a8aac12b43
Purnendu Kumar <purnendukumar8@hotmail.com>
2020-06-29 19:26:00 +0530

    cloned commit call baytpage object

--- jobportal/src/test/java/scripts/VerifyBayt.java
+++ jobportal/src/test/java/scripts/VerifyBayt.java
@@ -1,15 +1,17 @@
 package scripts;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import pageObjects.Baytpage;
 import utility.Base;
 
 public class VerifyBayt extends Base {
-	
+	WebDriver driver;
 	@Test
 	public void verifyBaytTest()
 	{
-		
+		Baytpage bp=new Baytpage(driver);
 	}
 
 }
